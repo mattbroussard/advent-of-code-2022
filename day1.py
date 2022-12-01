135dcd2875eaec8c08556040bc6789e16340a1cf
Matt Broussard <matt@mattb.io>
2022-11-30 21:59:46 -0800

    initial commit: day 1

---
+++ day1.py
@@ -0,0 +1,29 @@
+
+import sys
+
+def part1(elves):
+  sums = [sum(c) for c in elves]
+  return max(sums)
+
+def part2(elves):
+  sums = [sum(c) for c in elves]
+  sums.sort()
+  return sum(sums[-3:])
+
+def main():
+  fname = sys.argv[1]
+
+  elves = [[]]
+  with open(fname, 'r') as f:
+    for line in f:
+      if len(line.strip()) == 0:
+        elves.append([])
+        continue
+
+      elves[-1].append(int(line))
+
+  print("Part 1: %s" % (part1(elves),))
+  print("Part 2: %s" % (part2(elves),))
+
+if __name__ == '__main__':
+  main()
